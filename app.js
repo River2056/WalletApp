d43ef48249245ea872f54e1f5a4d4fbbe22275fa
River2056 <45826383+River2056@users.noreply.github.com>
2019-02-20 21:20:40 +0800

    refactored a whole lot more into JQuery!

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ function daysInMonth(year, month) {
 
 $(document).ready(function() {
     /**
-     * save button function
+     * save section
      */
+    $(".save_box .title").text("Enter your balance: ");
+    $("#act_bal").addClass("textbox");
+    $("#num_box").addClass("btn inline_block");
+    $("#num_box").val("save");
+    $("#save_msg").text("Done Saving!");
+    $("#save_msg").addClass("hide");
     $("#num_box").click(function() {
         var money = $("#act_bal").val();
         local.setItem("myAccount", money);
@@ -21,10 +27,17 @@ $(document).ready(function() {
         setTimeout(() => location.reload(), 2000);
 
     });
+    
 
     /**
-     * spent button function
+     * spent section
      */
+    $(".spent_box .title").text("Spent? ");
+    $("#spent").addClass("textbox");
+    $("#spent_btn").addClass("btn inline_block");
+    $("#spent_btn").val("spent");
+    $("#spent_msg").addClass("hide");
+    $("#spent_msg").text("Done Recording!");
     $("#spent_btn").click(function() {
         var spent = $("#spent").val();
         var account = local.getItem("myAccount");
@@ -37,18 +50,24 @@ $(document).ready(function() {
     });
 
     /**
-     * display account balance function
+     * display account balance section
      */
+    $(".balance_box .title").text("Show account balance: ");
+    $("#getbal_box").addClass("btn inline_block");
+    $("#getbal_box").val("show balance");
     $("#getbal_box").click(function() {
         var balance = local.getItem("myAccount");
         $("#get_bal").html(`Your account balance is: ${ balance }`)
     });
 
     /**
-     * calculate button function
+     * calculate button section
      * money icon on(green) => keep minimum savings == 3000
      * money icon off(white) => don't keep savings, calculate according storage number
      */
+    $("#cal_btn").addClass("btn");
+    $("#cal_btn").val("Calculate");
+    
     $("#cal_btn").click(function() {
         var left = local.getItem("myAccount");
         var check = $("#include_min").css("backgroundColor");
@@ -64,9 +83,10 @@ $(document).ready(function() {
     });
 
     /**
-     * change money icon background-color
+     * change money icon section
      * white <==> green
      */
+    $("#include_min").val("💵");
     $("#include_min").click(function() {
         var min = $("#include_min");
         if(min.css("backgroundColor") == "rgb(255, 255, 255)") { // if white
@@ -79,6 +99,8 @@ $(document).ready(function() {
     /**
      * reload button == F5
      */
+    $("#reload_btn").addClass("btn");
+    $("#reload_btn").val("reload page");
     $("#reload_btn").click(function() {
         location.reload();
     });
@@ -88,6 +110,8 @@ $(document).ready(function() {
      * pops a confirm window first, 
      * then clear storage
      */
+    $("#clear_btn").addClass("btn clr");
+    $("#clear_btn").val("🚨clear data!🚨");
     $("#clear_btn").click(function() {
         var check = window.confirm("Are you sure you want to clear data?");
         if (check) {
