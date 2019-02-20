ea0dc50173ea1059f053a8052133fd6bcaa10d32
River2056 <45826383+River2056@users.noreply.github.com>
2019-02-20 19:57:40 +0800

    refactored in JQuery

--- app.js
+++ app.js
@@ -2,68 +2,97 @@ var date = new Date();
 var month = date.getMonth();
 var year = date.getFullYear();
 
-var accountBalance = document.getElementById("act_bal"); // saves as a string
 var local = window.localStorage;
 
 function daysInMonth(year, month) {
-    return parseInt(new Date(year, month + 1 , 0).getDate());
+    return parseInt(new Date(year, month + 1, 0).getDate());
 }
 
-function getAccount() {
-    var balance = local.getItem("myAccount");
-    document.getElementById("get_bal").innerHTML = `Your account balance is: ${balance}`;
-}
+$(document).ready(function() {
+    /**
+     * save button function
+     */
+    $("#num_box").click(function() {
+        var money = $("#act_bal").val();
+        local.setItem("myAccount", money);
+        var saveMsg = $("#save_msg");
+        saveMsg.toggleClass("hide");
+        saveMsg.toggleClass("block");
+        setTimeout(() => location.reload(), 2000);
 
-function deduct() {
-    var spent = document.getElementById("spent").value;
-    var account = parseInt(local.getItem("myAccount"));
-    var newBalance = account - spent;
-    local.setItem("myAccount", newBalance);
-    var changeCSS = document.getElementById("spent_msg");
-    changeCSS.classList.remove("hide");
-    changeCSS.classList.add("block");
-    setTimeout(() => document.location.reload(), 2000);
-}
+    });
 
-function saveAccount() {
-    local.setItem("myAccount", accountBalance.value);
-    var changeCSS = document.getElementById("save_msg");
-    changeCSS.classList.remove("hide");
-    changeCSS.classList.add("block");
-    setTimeout(() => document.location.reload(), 2000);
-}
+    /**
+     * spent button function
+     */
+    $("#spent_btn").click(function() {
+        var spent = $("#spent").val();
+        var account = local.getItem("myAccount");
+        var newBalance = account - spent;
+        local.setItem("myAccount", newBalance);
+        var spentMsg = $("#spent_msg");
+        spentMsg.toggleClass("hide");
+        spentMsg.toggleClass("block");
+        setTimeout(() => location.reload(), 2000);
+    });
 
-function calculate() {
-    var left = parseInt(local.getItem("myAccount"));
-    var check = document.getElementById("include_min");
-    if(check.style.backgroundColor == "rgb(255, 255, 255)") {
-        left -= 0
-    } else {
-        left -= 3000;
-    }
-    var remain = daysInMonth(year, month);
-    remain = remain - date.getDate();
-    left = left / remain;
-    document.getElementById("cal_result").innerHTML = `${remain} days left! can only spend ${left} each day!`;
-}
+    /**
+     * display account balance function
+     */
+    $("#getbal_box").click(function() {
+        var balance = local.getItem("myAccount");
+        $("#get_bal").html(`Your account balance is: ${ balance }`)
+    });
 
-function changeColor() {
-    var min = document.getElementById("include_min");
+    /**
+     * calculate button function
+     * money icon on(green) => keep minimum savings == 3000
+     * money icon off(white) => don't keep savings, calculate according storage number
+     */
+    $("#cal_btn").click(function() {
+        var left = local.getItem("myAccount");
+        var check = $("#include_min").css("backgroundColor");
+        if(check == "rgb(255, 255, 255)") {
+            left -= 0;
+        } else if(check == "rgb(0, 255, 0)") {
+            left -= 3000;
+        }
+        var remain = daysInMonth(year, month);
+        remain -= date.getDate();
+        left /= remain;
+        $("#cal_result").html(`${remain} days left! can only spend ${left} each day!`);
+    });
 
-    if (min.style.backgroundColor == "rgb(255, 255, 255)") {
-        min.style.backgroundColor = "rgb(0, 255, 0)";
-    } else if (min.style.backgroundColor == "rgb(0, 255, 0)") {
-        min.style.backgroundColor = "rgb(255, 255, 255)";
-    }
-}
+    /**
+     * change money icon background-color
+     * white <==> green
+     */
+    $("#include_min").click(function() {
+        var min = $("#include_min");
+        if(min.css("backgroundColor") == "rgb(255, 255, 255)") { // if white
+            min.css("backgroundColor", "rgb(0, 255, 0)"); // change to green
+        } else if (min.css("backgroundColor") == "rgb(0, 255, 0)") { // if green
+            min.css("backgroundColor", "rgb(255, 255, 255)"); // change to white
+        }
+    });
 
-function clearStorage() {
-    var check = window.confirm("Are you sure you want to clear data?");
-    if(check) {
-        local.clear();
-    }
-}
+    /**
+     * reload button == F5
+     */
+    $("#reload_btn").click(function() {
+        location.reload();
+    });
 
-function reloadPage() {
-    document.location.reload();
-}
+    /**
+     * clear storage button
+     * pops a confirm window first, 
+     * then clear storage
+     */
+    $("#clear_btn").click(function() {
+        var check = window.confirm("Are you sure you want to clear data?");
+        if (check) {
+            local.clear();
+        }
+    });
+
+});
